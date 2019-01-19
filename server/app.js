0f5ae88e3200ffd461b5cfc63deaf7193df6e0cb
ced17i026 <ced17i026@iiitdm.ac.in>
2019-01-19 09:58:05 +0530

    only allow authorize user to delete or see message

--- server/app.js
+++ server/app.js
@@ -4,13 +4,15 @@ const express = require("express"),
     cors = require("cors"),
     bodyParser = require("body-parser"),
     authRoutes = require("./routes/authRoutes"),
+    authorize = require("./middleware/authenticate"),
     messageRoutes = require("./routes/messageRoutes"),
     error = require("./handlers/error");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 app.use("/api/user/",authRoutes);
-app.use("/api/user/:id/message",messageRoutes);
+app.use("/api/user/:id/message",
+authorize.isAuthenticated,authorize.isAuthorize,messageRoutes);
 app.use(error);
 app.listen(3001,function(){
     console.log("Server is running...");
--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,7 +1,45 @@
-exports.isAuthenticated = async function(){
-
+require("dotenv").load();
+const jwt = require("jsonwebtoken");
+exports.isAuthenticated = async function(req,res,next){
+    try{
+        let token = req.header.authorization.split(" ")[1];
+        jwt.verify(token,process.env.SECRET_KEY,function(err,decoded){
+            if(decoded){
+                return next();
+            }else{
+                return next({
+                    status: 401,
+                    message: "Please LogIn First",
+                })
+            }
+        });
+    }catch(err){
+        return next({
+            ...err,
+            status: 400,
+            message: "Unable to LogIn",
+        })
+    }
 }
 
-exports.isAuthorize = async function(){
-    
+exports.isAuthorize = async function(req,res,next){
+    try{
+        let token = req.header.authorization.split(" ")[1];
+        jwt.verify(token,process.env.SECRET_KEY,function(err,decoded){
+            if(decoded&&decoded._id === req.params.id){
+                return next();
+            }else{
+                return next({
+                    status: 401,
+                    message: "Unauthorize"
+                })
+            }
+        })
+    }catch(err){
+        return next({
+            ...err,
+            status: 401,
+            message: "Unauthorize",
+        })
+    }
 }
