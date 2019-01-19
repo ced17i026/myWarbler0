950cdd8e6c57e93b8ee3a6d92c5bce5e21665c63
ced17i026 <ced17i026@iiitdm.ac.in>
2019-01-19 17:58:04 +0530

    make authform form subbmission work

--- client/src/components/AuthForm.js
+++ client/src/components/AuthForm.js
@@ -8,9 +8,21 @@ class AuthForm extends Component{
             username:"",
             password: "",
         };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(e){
+        this.setState({
+            [e.target.name]:e.target.value,
+        });
+    }
+    handleSubmit(e){
+        e.preventDefault();
+
     }
     render(){
         const {heading} = this.props;
+        const {username,email,password} = this.state;
         return(
             <div className="AuthForm">
                 <h1>{heading}</h1>
@@ -18,21 +30,17 @@ class AuthForm extends Component{
                     {heading === "Sign Up"&&
                         <div className="form-group">
                             <label htmlFor="username">Username</label>
-                            <input type="text" className="form-control" id="username" placeholder="Enter Username"/>
+                            <input type="text" name="username" onChange={this.handleChange} value={username} className="form-control" id="username" placeholder="Enter Username"/>
                         </div>
                     }
                     <div className="form-group">
                         <label htmlFor="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                        <input type="email" className="form-control" onChange={this.handleChange}  id="exampleInputEmail1" name="email" vlaue={email} placeholder="Enter email"/>
                         <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
                     <div className="form-group">
                         <label htmlFor="exampleInputPassword1">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
-                    </div>
-                    <div className="form-group form-check">
-                        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                        <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                        <input type="password" className="form-control" onChange={this.handleChange}  name="password" value={password} id="exampleInputPassword1" placeholder="Password"/>
                     </div>
                     <button type="submit" className="btn btn-primary">{heading}</button>
                 </form>
