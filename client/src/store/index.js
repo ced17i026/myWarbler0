aebde7be8cc9febcac4193cd509049428db68b6a
ced17i026 <ced17i026@iiitdm.ac.in>
2019-01-19 11:34:58 +0530

    configure redux store

---
+++ client/src/store/index.js
@@ -0,0 +1,9 @@
+import rootReducer from "./reducers";
+import {createStore,applyMiddleware,compose} from "redux";
+import thunk from "redux-thunk";
+
+export const initializeStore = function(){
+    return store = createStore(rootReducer,compose(applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ));
+}
