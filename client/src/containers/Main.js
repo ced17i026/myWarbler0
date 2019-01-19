bc37174af342bb2ca00205648cc3193e354eaae1
ced17i026 <ced17i026@iiitdm.ac.in>
2019-01-19 17:35:16 +0530

    add login page

--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -2,12 +2,14 @@ import React,{Component} from "react";
 import {Route,withRouter,Switch} from "react-router-dom";
 import {connect} from "react-redux";
 import {Homepage} from "../components/Homepage";
+import AuthForm from "../components/AuthForm";
 
 class Main extends Component{
     render(){
         return (
             <Switch>
                 <Route exact path="/" render={(props)=><Homepage/>}/>
+                <Route exact path="/signup" render={(props)=><AuthForm />}/>
             </Switch>
         )
     }
